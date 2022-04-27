4a06d4461095b54025d12ade599bf2766d4cd84b
Exelzz <dogukan32145613@hotmail.com>
2022-04-27 14:48:44 +0300

    kod örnekleri

---
+++ Assets/Yarısma.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Yarısma : MonoBehaviour
+{
+    public Text soruismi, cevapa, cevapb, cevapc, cevapd;
+    Sorular sr;
+
+    public List<bool> sorulanlar;
+    public int cevap;
+
+
+    void Start()
+    {
+
+        sr = GetComponent<Sorular>();
+        for (int i = 0; i < sr.sorular.Count; i++)
+        {
+            sorulanlar.Add(false);
+        }
+        SoruEkle();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void SoruEkle()
+    {
+        for (int i = 0; i < sorulanlar.Count; i++)
+        {
+            if (sorulanlar[i] == false)
+            {
+                int sorusayi = Random.Range(0, sorulanlar.Count);
+                if (sorulanlar[sorusayi] == false)
+                {
+                    sorulanlar[sorusayi] = true;
+                    soruismi.text = sr.sorular[sorusayi].soruismi;
+                    cevapa.text = sr.sorular[sorusayi].cevapa;
+                    cevapb.text = sr.sorular[sorusayi].cevapb;
+                    cevapc.text = sr.sorular[sorusayi].cevapc;
+                    cevapd.text = sr.sorular[sorusayi].cevapd;
+                    cevap = sr.sorular[sorusayi].cevap;
+                }
+                else
+                {
+                    SoruEkle();
+                }
+                break;
+            }
+            if (i == sorulanlar.Count - 1)
+            {
+                Debug.Log("Bitti");
+            }
+        }
+
+
+
+    }
+
+    public void CevapVer(int deger)
+    {
+        if (deger == cevap)
+        {
+            SoruEkle();
+        }
+        else
+        {
+            Debug.Log("Yanlış Cevap");
+        }
+    }
+}
+
+
